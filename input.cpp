cf8d7be91a817f97aba13e7d85e1e36188ee00ca
Kamil Warchoł <kamil.warchol.93@gmail.com>
2017-10-31 23:24:04 +0100

    Modify input functions and move them to input.h

---
+++ input.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include "input.h"
+
+void readAnyKey()
+{
+    using std::cout;
+
+    cout << "Press ENTER to continue...\n";
+    // make sure we only have one blank line between user input
+    // and next program output, regardless of whether the user
+    // just pressed ENTER or typed in some characters before
+    bool anyInput = false;
+    while (std::cin.get() != '\n')
+        anyInput = true;
+
+    if (anyInput)
+        cout << std::endl;
+}
+
+char readKey(const char * availableOptions, int optionsCount)
+{
+    using std::cout;
+
+    cout << "Your choice: ";
+    bool wrongChoice = true;
+    char choice;
+    while (wrongChoice)
+    {
+        choice = readChar();
+        for (int i = 0; i < optionsCount; i++)
+            if (choice == availableOptions[i])
+            {
+                wrongChoice = false;
+                break;
+            }
+
+        if (wrongChoice)
+            cout << "\aTry again: ";
+    }
+    cout << std::endl;
+
+    return choice;
+}
+
+char readChar()
+{
+    char ch;
+    while ((ch = std::cin.get()) == '\n')
+        continue;
+    flushInput();
+
+    return ch;
+}
+
+void flushInput()
+{
+    using std::cin;
+
+    cin.clear();
+    while (cin.get() != '\n')
+        continue;
+}
---
+++ input.h
@@ -0,0 +1,7 @@
+#pragma once
+
+void readAnyKey();
+char readKey(const char * availableOptions, int optionsCount);
+
+char readChar();
+void flushInput();
