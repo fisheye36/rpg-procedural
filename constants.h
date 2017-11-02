b83903026019d4a039a870fb19a18d22ae9c711e
Kamil Warchoł <kamil.warchol.93@gmail.com>
2017-11-02 16:34:49 +0100

    Move define constants to constants.h

---
+++ constants.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define STARTING_PTS_PLAYER 20
+#define STARTING_PTS_MONSTER 15
+#define PTS_PER_LVL 3
+
+#define SKILLS_COUNT 5
+#define ID 0
+#define NA 1
+#define ND 2
+#define MA 3
+#define MD 4
+#define HP 5
