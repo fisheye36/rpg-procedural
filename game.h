014169da2c4f872b3cedcacd38a0869f387fe1df
Kamil Warchoł <kamil.warchol.93@gmail.com>
2017-11-02 13:16:36 +0100

    Move character generation functions to character.h

--- game.h
+++ game.h
@@ -1,26 +1,3 @@
 #pragma once
 
-#include <string>
-
-#define SKILLS_COUNT 5
-#define ID 0
-#define NA 1
-#define ND 2
-#define MA 3
-#define MD 4
-#define HP 5
-
-typedef int skill;
-typedef skill * charSkills;
-
 void startGame();
-
-charSkills createCharacter(bool player = false);
-charSkills createCharacter(std::string & playerName);
-void deleteCharacters();
-
-void createPlayer(charSkills player);
-void displaySkills(const charSkills character);
-bool changeSkill(charSkills character, int & availableSkillPoints);
-
-std::string getPlayerName();
