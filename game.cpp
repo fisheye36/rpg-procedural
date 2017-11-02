44cd9df1d2e786b91c669e6f99fe9d5149898bbd
Kamil Warchoł <kamil.warchol.93@gmail.com>
2017-11-02 22:00:11 +0100

    Implement gameOver()

--- game.cpp
+++ game.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <cstdlib>
 
 #include "game.h"
 #include "character.h"
 #include "fight.h"
+#include "input.h"
 #include "constants.h"
+
+extern std::vector<charSkills> g_characters;
 
 void startGame()
 {
@@ -29,6 +33,30 @@ void startGame()
             buildPlayer(player, PTS_PER_LVL);
         }
     }
-    //gameOver();
+    gameOver(player, playerName);
     deleteCharacters();
+}
+
+void gameOver(charSkills player, std::string playerName)
+{
+    using std::cout;
+    using std::endl;
+
+    cout << playerName << ", you are DEAD...\n"
+            "Before you died, you have managed to kill " << g_characters.size() - 2
+         << " monsters.\nBelow you can see your final stats:\n\n"
+         << playerName << endl;
+    displaySkills(player);
+
+    if (g_characters.size() > 2)
+    {
+        cout << "And here is a complete list of every monster you have slaughtered:\n\n";
+        for (unsigned i = 1u; i < g_characters.size() - 1u; i++)
+        {
+            readAnyKey();
+            cout << "Monster #" << g_characters[i][ID] << endl;
+            displaySkills(g_characters[i]);
+        }
+        readAnyKey();
+    }
 }
--- game.h
+++ game.h
@@ -1,3 +1,7 @@
 #pragma once
 
+#include <string>
+#include "character.h"
+
 void startGame();
+void gameOver(charSkills player, std::string playerName);
