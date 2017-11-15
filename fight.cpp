933ad5514bcd2c2f70ea30ad0e0f1a6c39e75590
Kamil Warchoł <kamil.warchol.93@gmail.com>
2017-11-15 22:52:56 +0100

    Pause the game when printing fight output

--- fight.cpp
+++ fight.cpp
@@ -14,6 +14,7 @@ void fight(charSkills player, charSkills monster)
     bool charDead;
     do
     {
+        readAnyKey();
         if (playerTurn)
             attack = playerAttack;
         else
